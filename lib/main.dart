843250dd064049af6412b893f9f2f5ade766d282
안가을 <autumnly1007@gmail.com>
2024-03-10 08:22:35 +0900

    typography

--- lib/main.dart
+++ lib/main.dart
@@ -14,22 +14,16 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title:Text('야옹앱', style: TextStyle(color: Colors.white)),
           backgroundColor: Colors.pinkAccent,
+          leading: Icon(Icons.star),
+          actions: [Icon(Icons.star), Icon(Icons.star), Icon(Icons.star)],
         ),
-        body: Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            width: double.infinity, // 부모 박스 영역 모두 채우기
-            height: 50,
-            // color: Colors.pinkAccent, 에러. 같은 속성은 한 번만 작성해야 함
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-            decoration: BoxDecoration(
-              color: Colors.pinkAccent,
-              border: Border.all(color: Colors.black)
-            ),
-            child: Text('냥냥펀치!')
-          ),
-        )
+        body: SizedBox(
+          child:
+            ElevatedButton(
+              child: Text('야옹'),
+              onPressed: (){},
+            )
+        ),
       )
     );
   }
