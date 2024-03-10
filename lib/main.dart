d52eae94ce805ce6b5e5ecfd08c211b042a8e54d
안가을 <autumnly1007@gmail.com>
2024-03-10 09:35:58 +0900

    list item

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,22 +11,59 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title:Text('야옹앱', style: TextStyle(color: Colors.white)),
-          backgroundColor: Colors.pinkAccent,
-          leading: Icon(Icons.star),
-          actions: [Icon(Icons.star), Icon(Icons.star), Icon(Icons.star)],
+  return MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text('관악구', style: TextStyle(fontWeight: FontWeight.w700)),
+              Icon(Icons.keyboard_arrow_down)
+          ],
         ),
-        body: SizedBox(
-          child:
-            ElevatedButton(
-              child: Text('야옹'),
-              onPressed: (){},
-            )
+        backgroundColor: Colors.amberAccent,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1.0),
+          child: Container(
+            color: Colors.grey,
+            height: 1,
+          ),
         ),
+      ),
+      body: Container(
+        height: 150,
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children:[
+            Image.asset('cat-ball.jpg', width: 150, height: 150),
+            SizedBox(width: 10),
+            Expanded(
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('귀여운 고양이 구경만 하세요', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20)),
+                    Text('도림천 옆', style: TextStyle(color: Colors.grey)),
+                    Text('500,000,000 원', style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16)),
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Icon(Icons.favorite, color: Colors.red, size: 20),
+                          SizedBox(width: 3),
+                          Text('32', style: TextStyle(fontWeight: FontWeight.w600)),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        )
       )
-    );
+    )
+  );
   }
 }
